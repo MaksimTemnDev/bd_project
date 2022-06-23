c150cc9cf1e1d07f146c91838006863339edecaf
MaksimTemnDev <79454370+MaksimTemnDev@users.noreply.github.com>
2022-06-23 12:45:37 +0300

    Update bd_py_.py
    add new funtions: show add params show tables

--- bd_py_.py
+++ bd_py_.py
@@ -20,6 +20,246 @@ def create_connection():
         print(e)
     return connection
 
+def show_all():
+        show_rows = "SHOW TABLES FROM `origin`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Tables list:")
+        print('#'*25)
+        for row in rows:
+            print(row['Tables_in_origin'])
+            i+=1
+        print('#'*25)
+        print(" ")
+
+def find_in_base(name):
+    has_strt = 0
+    if name == "auto":
+        show_rows = "SELECT * FROM `origin`.`auto`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print("Unicue number")
+        print('#'*25)
+        for row in rows:
+            print(row["UnicueNumber"])
+            i+=1
+        print('#'*25)
+    elif name == "brand":
+        show_rows = "SELECT * FROM `origin`.`brand`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Denomination")
+        print('#'*25)
+        for row in rows:
+            print(row["denomination"])
+            i+=1
+        print('#'*25)
+    elif name == "client":
+        show_rows = "SELECT * FROM `origin`.`client`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Name")
+        print('#'*25)
+        for row in rows:
+            print(row["name"])
+            i+=1
+        print('#'*25)
+    elif name == "manager":
+        show_rows = "SELECT * FROM `origin`.`manager`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Name")
+        print('#'*25)
+        for row in rows:
+            print(row["name"])
+            i+=1
+        print('#'*25)
+    elif name == "equipment":
+        show_rows = "SELECT * FROM `origin`.`equipment`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Cabin    Wheels    Transmission    Carcase    Climatic control")
+        print('#'*25)
+        for row in rows:
+            print(row["cabin"], row["wheels"], row["transmission"], row["carcase"], row["climatic_control"])
+            i+=1
+        print('#'*25)
+    elif name == "model":
+        show_rows = "SELECT * FROM `origin`.`model`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Denomination")
+        print('#'*25)
+        for row in rows:
+            print(row["denomination"])
+            i+=1
+        print('#'*25)
+    elif name == "position":
+        show_rows = "SELECT * FROM `origin`.`position`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Denomination")
+        print('#'*25)
+        for row in rows:
+            print(row["denomenation"])
+            i+=1
+        print('#'*25)
+    elif name == "delivery":
+        show_rows = "SELECT * FROM `origin`.`delivery`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Date")
+        print('#'*25)
+        for row in rows:
+            print(row["date"])
+            i+=1
+        print('#'*25)
+    elif name == "deal":
+        show_rows = "SELECT * FROM `origin`.`deal`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Date")
+        print('#'*25)
+        for row in rows:
+            print(row["dateOfIssue"])
+            i+=1
+        print('#'*25)
+    elif name == "engine":
+        show_rows = "SELECT * FROM `origin`.`engine`" 
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Fuel    Part number   Power")
+        print('#'*25)
+        for row in rows:
+            print(row["fuel"], row["partNumber"], row["horsePower"])
+            i+=1
+        print('#'*25)
+    elif name == "tipeofpayment":
+        show_rows = "SELECT * FROM `origin`.`tipeofpayment`"
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        i = 1;
+        print(" ")
+        print("Type of pay   Manager bonus   Procent of sale")
+        print('#'*25)
+        for row in rows:
+            print(row["denomination"], row["SizeOfManagersBonus"], row["ProcentOfSale"])
+            i+=1
+        print('#'*25)
+    print(" ")
+
+def change_table(naming):
+    if naming == "1":
+        show_rows = "SHOW COLUMNS FROM `origin`.brand"
+        cursor.execute(show_rows)
+        print(" ")
+        print("List of columns")
+        print('#'*25)
+        print("denomination")
+        print('#'*25)
+        print(" ")
+        print("Введите новое значения denomination:")
+        condition1 = input();
+        print("Введите условие:")
+        mean = input()
+        try:
+            set_update = "update brand set denomination = '%s' where %s"%(condition1, mean)
+            cursor.execute(set_update)
+            connect.commit()
+        except Exception  as e:
+            print("Ваш запрос синтаксически не правильный")
+            print(e)
+            print(" ")
+    elif naming == "2":
+        show_rows = "SHOW COLUMNS FROM `origin`.model"
+        cursor.execute(show_rows)
+        print(" ")
+        print("List of columns")
+        print('#'*25)
+        print("denomination")
+        print('#'*25)
+        print(" ")
+        print("Выберите что изменять 1-denomination")
+        var = input()
+        if(var ==  '1'):
+            print("Введите новое значения denomination:")
+            condition1 = input();
+            print("Введите условие:")
+            mean = input()
+            try:
+                set_update = "update model set denomination = '%s' where %s"%(condition1, mean)
+                cursor.execute(set_update)
+                connect.commit()
+            except Exception  as e:
+                print("Ваш запрос синтаксически не правильный")
+                print(e)
+                print(" ")
+
+def inserting_table(inp, man_id):
+    if inp == "1":
+        show_rows = "SHOW COLUMNS FROM `origin`.client"
+        cursor.execute(show_rows)
+        print(" ")
+        print("List of columns")
+        print('#'*25)
+        print("name")
+        print('#'*25)
+        print(" ")
+        print("Введите значения для новой записи в том же порядке что и указан в списке столбцов:")
+        name = input()
+        clientId = man_id
+        try:
+                set_update = "insert into origin.client(name, clientId) VALUES('%s',%s);"%(name, clientId)
+                cursor.execute(set_update)
+                man_id+=1
+                connect.commit()
+        except Exception  as e:
+                print("Ваш запрос синтаксически не правильный")
+                print(e)
+                print(" ")
+    elif inp == '2':
+        show_rows = "SHOW COLUMNS FROM `origin`.manager"
+        cursor.execute(show_rows)
+        print(" ")
+        print("List of columns")
+        print('#'*25)
+        print("name position")
+        print('#'*25)
+        print(" ")
+        print("Введите значения для новой записи в том же порядке что и указан в списке столбцов:")
+        name = input()
+        positionId = input()
+        try:
+                set_update = "insert into origin.manager(name, managerId, positionId) VALUES('%s',%s,%s);"%(name, man_id, positionId)
+                man_id+=1
+                cursor.execute(set_update)
+                connect.commit()
+        except Exception  as e:
+                print("Ваш запрос синтаксически не правильный")
+                print(e)
+                print(" ")
+        
 
 play = 1
 man_id = 11
