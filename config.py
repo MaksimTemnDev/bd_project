9873fbc89b9820aa002672b311cdaf55790d39a5
MaksimTemnDev <79454370+MaksimTemnDev@users.noreply.github.com>
2022-06-24 08:26:58 +0300

    Update config.py
    new class wich works with database

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
+import pymysql
+
 host = "127.0.0.1"
 user = "root"
 password = "1234"
 db_name = "origin"
+
+class BaseControl:
+    @staticmethod
+    def rows(show_rows, cursor):
+        cursor.execute(show_rows)
+        rows = cursor.fetchall()
+        return rows
